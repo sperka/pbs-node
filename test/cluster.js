333bb81e38613b3f9c1bdb6e8351206984cb0980
Greg Varga <spherule@gmail.com>
2012-05-21 13:14:06 -0500

    added some tests; config added

--- test/cluster.js
+++ test/cluster.js
@@ -5,19 +5,35 @@
  */
 
 var Cluster = require('../lib/cluster')
-		, should = require('should');
+	, Machine = require('../lib/machine')
+	, should = require('should');
+
+require('../lib/helpers');
 
 describe('Cluster', function() {
 	it('Should initialize', function(done) {
 
+		var cluster = new Cluster();
+		should.exist(cluster);
+		cluster.nodes.should.be.an.instanceof(Array);
+		cluster.nodes.length.should.equal(0);
+
 		done();
 	});
 
-	it('Should get nodes', function(done) {
-		Cluster.getNodes(function(err, result) {
+	it('Should get cluster', function(done) {
+		Cluster.getCluster(function(err, result) {
 			should.not.exist(err);
 
+			result.should.be.an.instanceof(Cluster);
+			result.nodes.should.be.an.instanceof(Array);
+			result.nodes.length.should.equal(2);
+
+			var node = result.nodes[0];
+			node.should.be.an.instanceof(Machine);
+
 			console.log(result);
+			done();
 		});
 	});
 });
