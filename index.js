45bd0a8f7a3435ecdc515992923d8d8757077a00
Greg Varga <spherule@gmail.com>
2012-05-21 00:24:33 -0500

    stat, qstat-f, qsub, qnodes; additional changes

--- index.js
+++ index.js
@@ -4,18 +4,30 @@
  * Time: 8:47 PM
  */
 
+// 'register' convenience methods
+require('./lib/helpers');
+
 var pbs			= require('./lib/pbs')
 	, Cluster	= pbs.Cluster
 	, Job		= pbs.Job;
 
 /*
-Cluster.getNodes(function(err, result) {
+Cluster.getCluster(function(err, result) {
 	console.log('Err: ' + err);
 	console.log('Result: ' + JSON.stringify(result));
 });
-*/
 
 Job.searchJobsByName('IL', false, function(err, result) {
+	console.log('Err: ' + err);
+	console.log('Result: ' + JSON.stringify(result));
+});*/
+
+Job.getJobById('jobID', function(err, result) {
+	console.log('Err: ' + err);
+	console.log('Result: ' + JSON.stringify(result));
+});
+
+Job.getJobStatus('jobID', function(err, result) {
 	console.log('Err: ' + err);
 	console.log('Result: ' + JSON.stringify(result));
 });
